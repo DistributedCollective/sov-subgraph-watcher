4023f6da66f15f1c436ecfcdf25da750a2636022
Rytis Grincevičius <rytis.grincevicius@gmail.com>
2023-09-07 09:53:03 +0300

    fix: remove database string print

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,9 +3,5 @@ set -e
 
 export DATABASE_URL="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
 
-echo $DATABASE_URL
-
-ls -la
-
 yarn prisma migrate deploy || yarn prisma migrate reset --force
 yarn start
